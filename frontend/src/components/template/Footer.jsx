28b941824389e53a481e4dad15cb76624224a6ef
VictorKiill <victorkiill@gmail.com>
2022-04-11 19:45:39 -0300

    AppCreated

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = props =>
+    <footer className="footer">
+       <span>
+           Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+           <strong> Victor Kiill</strong>
+       </span>
+    </footer>
+
+export default Footer
---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Nav.css'
+
+const Nav = props =>
+    <aside className="menu-area">
+        <nav className="menu">
+            <a href="/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
+    </aside>
+
+export default Nav
